2d32fa19a1de08eb86cf9d66c4c14545763408a5
Aron Racho <aron.racho@jivesoftware.com>
2013-04-13 14:17:03 -0700

    Cleanup

--- autowire/bootstrap.js
+++ autowire/bootstrap.js
@@ -1,12 +1,26 @@
-var express = require('express')
-    , fs = require('fs')
-    , http = require('http')
-    , path = require('path')
-    , jive = require('../api')
-    , tileConfigurator = require('./tileConfigurator')
-    , appConfigurator = require('./appConfigurator')
-    , consolidate = require('consolidate')
-;
+/*
+ * Copyright 2013 Jive Software
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+var express = require('express'),
+    fs = require('fs'),
+    path = require('path'),
+    jive = require('../api'),
+    tileConfigurator = require('./tileConfigurator'),
+    appConfigurator = require('./appConfigurator'),
+    consolidate = require('consolidate');
 
 exports.start = function( app, rootDir ) {
 
