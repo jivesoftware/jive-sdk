f89d50a2c0dd6f91177d8aaabb1340b1ee74d612
Jesse Hallett <jesse.hallett@jivesoftware.com>
2013-04-22 16:40:13 -0700

    Adds example tile that displays bitcoin exchange rates

---
+++ service/generator/examples/bitcoin/tiles/bitcoin/backend/services.js
@@ -0,0 +1,79 @@
+/*jshint laxcomma:true */
+
+var jive = require('jive-sdk')
+  , q    = require('q')
+  , http = require('q-io/http');
+
+exports.task = new jive.tasks.build(function() {
+    var tiles = jive.tiles.findByDefinitionName('bitcoin').then(function(instances) {
+        return instances || [];
+    });
+    var prices = tiles.then(function(ts) {
+        return ts.length ? fetchPrices() : q.deferred({});
+    });
+    q.all([tiles, prices]).then(function(results) {
+        var ts = results[0]
+          , ps = results[1];
+        ts.forEach(pushUpdate.bind(null, ps));
+    });
+}, 600000);
+
+function fetchPrices() {
+    var pricesUrl = 'http://api.bitcoincharts.com/v1/weighted_prices.json';
+    return http.read(pricesUrl).then(JSON.parse);
+}
+
+function pushUpdate(prices, tile) {
+    console.log('pushing update: '+ tile.name +', '+ tile.id);
+    var symbol   = tile.config.symbol || 'USD';
+    var myPrices = prices[symbol];
+
+    var data = {
+        data: {
+            "title" : "Bitcoin Exchange Rates",
+            "contents" : ['24h', '7d', '30d'].map(function(t) {
+                return { name: t, value: myPrices[t] + ' ' + symbol };
+            }),
+            "action" : {
+                "text" : "bitcoincharts.com",
+                "url"  : "http://bitcoincharts.com/"
+            }
+        }
+    };
+
+    jive.tiles.pushData(tile, data);
+}
+
+exports.eventHandlers = [
+
+    {
+        'event': 'newInstance',
+        'handler' : function(theInstance){
+            fetchPrices().then(function(prices) {
+                pushUpdate(prices, theInstance);
+            });
+        }
+    },
+
+    {
+        'event': 'destroyingInstance',
+        'handler' : function(theInstance){
+            // override
+        }
+    },
+
+    {
+        'event': 'destroyedInstance',
+        'handler' : function(theInstance){
+            // override
+        }
+    },
+
+    {
+        'event': 'dataPushed',
+        'handler' : function(theInstance, pushedData, response){
+            // override
+        }
+    }
+
+];
---
+++ service/generator/examples/bitcoin/tiles/bitcoin/public/main.js
@@ -0,0 +1,36 @@
+jive.tile.onOpen(function(data, options) {
+    var $form = $('form');
+
+    $form.find('input[value="'+ data.symbol +'"]').prop('checked', true);
+
+    $form.bind('submit', function(event) {
+        event.preventDefault();
+        var param = $(this).serializeArray().filter(function(p) {
+            return p.name === 'symbol';
+        })[0];
+        var symbol = (param && param.value) || 'USD';
+        jive.tile.close({ symbol: symbol }, resetData());
+    });
+
+    gadgets.window.adjustHeight();
+
+    function resetData() {
+        options.definitions.filter(function(d) {
+            return d.name === 'bitcoin';
+        }).forEach(function(def) {
+            (def.data.contents || []).forEach(function(c) {
+                c.value = '--';
+            });
+        });
+        return options.definitions;
+        //var def = options.definitions.filter(function(d) {
+        //    return d.name === 'bitcoin';
+        //})[0];
+        //if (def) {
+        //    $.each(def.data.contents, function() {
+        //        this.value = '--';
+        //    });
+        //    return def.data;
+        //}
+    }
+});
