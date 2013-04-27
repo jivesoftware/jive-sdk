c69b5877c7a9ad4dc297149111f23e4e32e0d043
Aron Racho <aron.racho@jivesoftware.com>
2013-04-27 00:15:44 -0700

    More oauth2

--- service/generator/examples/samplesfdc/tiles/samplesfdc/backend/routes/oauth/sampleOauth.js
+++ service/generator/examples/samplesfdc/tiles/samplesfdc/backend/routes/oauth/sampleOauth.js
@@ -13,6 +13,10 @@ var tokenStore = jive.service.persistence();
 // overrides jive-sdk/routes/oauth.js to do something useful,
 // like storing access token for the viewer
 
+exports.fetchOAuth2Conf = function() {
+    return jive.service.options['oauth2-sfdc'];
+};
+
 myOauth.oauth2SuccessCallback = function( state, originServerAccessTokenResponse, callback ) {
     console.log('State', state);
     console.log('originServerAccessTokenResponse', originServerAccessTokenResponse);
