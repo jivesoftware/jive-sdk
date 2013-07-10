9ac92c8a2bc5e9d211ad5d9dfbe5eea29c75cac2
Aron Racho <aron.racho@jivesoftware.com>
2013-07-10 15:12:31 -0700

    Starting to add client app oauth support

--- service/jiveOAuth.js
+++ service/jiveOAuth.js
@@ -4,20 +4,32 @@ var q = require('q');
 
 exports.register = function( registration ) {
     var deferred = q.defer();
-    var authorizationCode = registration['code'];
-    var scope = registration['scope'];
-    var tenantId = registration['tenantId'];
-    var jiveUrl = registration['jiveUrl'];
-    var clientId = registration['clientId'];
     var jiveSignature = registration['jiveSignature'];
 
     // xxx todo - validate jiveSignature, then do access token exchange
 
     var registrationToSave = JSON.parse( JSON.stringify(registration) );
 
+    var authorizationCode = registration['code'];
+    var scope = registration['scope'];
+    var tenantId = registration['tenantId'];
+    var jiveUrl = registration['jiveUrl'];
+    var clientId = registration['clientId'];
+    var clientSecret = registration['clientSecret'];
+
+    if ( !clientId ) {
+        // use global one
+        clientId = jive.service.options['clientId'];
+    }
+    if ( !clientSecret ) {
+        // use global one
+        clientSecret = jive.service.options['clientSecret'];
+    }
+
     // do access token exchange
     client.requestAccessToken(
         {
+            'client_secret' : clientSecret,
             'client_id' : clientId,
             'code' : authorizationCode,
             'jiveUrl' : jiveUrl,
