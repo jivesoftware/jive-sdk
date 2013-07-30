7e0cf895b92330ea041b8204321bed342d4131c3
Aron Racho <aron.racho@jivesoftware.com>
2013-07-29 19:40:02 -0700

    allow single node to be both pusher and worker

--- jive-sdk-api/lib/scheduler/kue/scheduler.js
+++ jive-sdk-api/lib/scheduler/kue/scheduler.js
@@ -5,7 +5,7 @@
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/x    licenses/LICENSE-2.0
  *
  *    Unless required by applicable law or agreed to in writing, software
  *    distributed under the License is distributed on an "AS IS" BASIS,
@@ -111,7 +111,8 @@ Scheduler.prototype.init = function init( _eventHandlerMap, options ) {
     if ( isWorker  ) {
         new worker().init(jobQueueName, _eventHandlerMap);
     }
-    else if ( isPusher  ) {
+
+    if ( isPusher  ) {
         new worker().init(pushQueueName, _eventHandlerMap);
     }
 
