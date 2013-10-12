a6549dab67b43b6ee4e0af788ef92113ebf446ed
Aron Racho <aron.racho@jivesoftware.com>
2013-10-12 15:20:19 -0700

    fixing webhooks example

--- jive-sdk-service/generator/examples/samplewebhooks/services/webhooks/backend/routes/explicit_routes.js
+++ jive-sdk-service/generator/examples/samplewebhooks/services/webhooks/backend/routes/explicit_routes.js
@@ -1,29 +1,26 @@
+var jive = require('jive-sdk');
+var fs = require('fs');
+
 exports.getWebhooksLog = {
     'path' : '/webhooks',
     'verb' : 'get',
     'route': function(req, res) {
-        var activityList = req.body;
+        var raw = '<head> <link rel="stylesheet" type="text/css" href="' + jive.service.serviceURL() + '/stylesheets/style.css"></head>';
+        raw += '<h1>Webhooked Activity</h1>';
 
-        if ( activityList && activityList['forEach'] ) {
-            activityList.forEach( function(activity) {
-                console.log('->', activity);
+        jive.util.fsexists('webhooks.log').then(function(exists){
+            if ( !exists ) {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end( raw + "No data." );
+            } else {
+                jive.util.fsread('webhooks.log').then( function( data ) {
+                    raw += data.toString();
 
-                var toAppend = activity['activity']['content'];
-
-                if ( activity['activity'] && activity['activity']['provider'] && activity['activity']['provider']['url'] ) {
-                    toAppend += " @ <b>" + activity['activity']['provider']['url'] + "</b>"
-                }
-
-                toAppend += '<br>\n';
-
-                fs.appendFile('webhooks.log', toAppend, function(err) {
-                    console.log(err);
-                });
-            });
-        }
-
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end( JSON.stringify( { } ) );
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.end( raw );
+                })
+            }
+        });
     }
 };
 
@@ -45,7 +42,7 @@ exports.postWebhooks = {
 
                 toAppend += '<br>\n';
 
-                require('fs').appendFile('webhooks.log', toAppend, function(err) {
+                fs.appendFile('webhooks.log', toAppend, function(err) {
                     console.log(err);
                 });
             });
