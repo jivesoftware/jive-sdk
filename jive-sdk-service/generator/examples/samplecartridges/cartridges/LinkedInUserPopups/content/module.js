293f57aff07141d0f145cd90c1fe75fa284564e4
Aron Racho <aron.racho@jivesoftware.com>
2013-10-13 09:12:47 -0700

    added 2 more cartridges

---
+++ jive-sdk-service/generator/examples/samplecartridges/cartridges/LinkedInUserPopups/content/module.js
@@ -0,0 +1,42 @@
+﻿/* LinkedIn module for Jive Anywhere
+
+   ModuleContext, JCommon and $ are provided by the module manager
+   Recommended URL filter: linkedin.com 
+*/
+
+this.minimumRequiredVersion = 2.1;
+
+this.init = function () {
+    // tells pagescript.js to inject hooks required for monitoring the user popup
+    ModuleContext.runPageScript("init", null);
+
+    // compile markup as named templates
+    var pageDataTemplate = ModuleContext.getResourceFile("LinkedInPageDataTemplate.html");
+
+    $.templates({
+        linkedInPageDataTemplate: pageDataTemplate
+    });
+};
+
+this.onMessage = function (command, data, sendResponse) {
+    if (command == "getProfile") {
+        // reveived a getProfile message from pagescript.js, search for a user matching link name
+        ModuleContext.connectionContexts.activeConnection.clientFacade.searchUsers(data, 1, function (result) {
+            if (result.people && result.people.length == 1) {
+                // one user found, get the user id
+                var userId = result.people[0].id;
+                // request full profile
+                ModuleContext.connectionContexts.activeConnection.clientFacade.getUserById(userId, function (user) {
+                    // render the template of the Jive section and return its html
+                    var html = $.render.linkedInPageDataTemplate(user);
+                    sendResponse(html);
+                });
+            }
+            else {
+                // user not found, render the template with empty data and return its html
+                var html = $.render.linkedInPageDataTemplate({ isError: result.isError, message: result.message });
+                sendResponse(html);
+            }
+        });
+    }
+};
---
+++ jive-sdk-service/generator/examples/samplecartridges/cartridges/LinkedInUserPopups/content/pagescript.js
@@ -0,0 +1,46 @@
+﻿/* LinkedIn module for Jive Anywhere (page content script)
+
+   PageModuleContext, JCommon and $ are provided by the module manager
+*/
+
+var isTooltipVisible = false;
+
+var checkPopupVisibility = function () {
+    var popup = $("#lui-mini-profile-body");
+    var name = $(".name > a", popup).text();
+
+    if (popup.length == 1 && popup.css("display") != "none" && name) {
+        // visible popup found
+        if (!isTooltipVisible) {
+            // current monitored visibility flag has changed
+            isTooltipVisible = true;
+            //console.log("Invoking LinkedIn profile search for Jive user: " + name);
+
+            // send a message to module.js (background context) for getting rendered html for hovered user
+            PageModuleContext.sendMessage("getProfile", name, function (html) {
+
+                // create DOM object from received HTML string
+                var jiveSection = $.parseHTML(html);
+
+                // create the container if it doesn't exist
+                var container = $(".miniprofile-shared-connections", popup);
+                if (container.length == 0) {
+                    $(".miniprofile-body", popup).after('<div class="miniprofile-shared-connections"></div>');
+                }
+
+                // append templated HTML into the popup and start animation
+                container.prepend(jiveSection);
+                $(jiveSection).slideDown();
+            });
+        }
+    }
+    else {
+        // clear the visibility flag
+        isTooltipVisible = false;
+    }
+};
+
+this.init = function (params, sendResponse) {
+    // create a timer interval that checks if the user popup visibility has changed each 0.5 seconds
+    window.setInterval(checkPopupVisibility, 500);
+};
