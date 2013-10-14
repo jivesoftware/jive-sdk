21646af52895f31f031d3a004ee1132c56c61410
Aaron Malenfant <aaron.malenfant@jivesoftware.com>
2013-10-14 15:06:49 -0700

    Added error handling and fixed a bug with spaces in project names and saving on the detail page.

--- jive-sdk-service/generator/examples/todo/apps/todo/public/scripts/shared.js
+++ jive-sdk-service/generator/examples/todo/apps/todo/public/scripts/shared.js
@@ -1,12 +1,16 @@
 var host, app, baseRESTUrl;
 
 if( !host ) {
-    var url = gadgets.util.getUrlParameters()['url'];
-    if( url ) {
+    var appXMLUrl = gadgets.util.getUrlParameters()['url'];
+    if( appXMLUrl ) {
         // parts will look like: ["http:", "", "localhost:8090", "osapp", "todo", "app.xml"]
-        var parts  = url.split("/");
+        var parts  = appXMLUrl.split("/");
         host = parts[0] + "//" + parts[2];
         app = parts[4];
         baseRESTUrl = host + "/" + app + "/todos";
     }
+}
+
+var defaultErrorHandler = function(response) {
+    $("#status").html("ERROR:" + response.error.message + "(" + response.error.code + ")");
 }
