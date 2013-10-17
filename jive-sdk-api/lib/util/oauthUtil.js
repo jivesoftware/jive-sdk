78a307b44bbb36a92419144820eb365848137004
shali3 <shali.shaltiel@jivesoftware.com>
2013-10-17 19:17:03 +0300

    added requestAccessToken to the oauthUtil

--- jive-sdk-api/lib/util/oauthUtil.js
+++ jive-sdk-api/lib/util/oauthUtil.js
@@ -1,20 +1,52 @@
+var q = require('q');
 var url = require('url');
 var util = require('util');
 var jive = require('../../api');
+var tileClient = require('../tile/client');
+var community = require('../community/community');
 
-exports.buildAuthorizeUrlResponseMap = function( oauth2Conf, callback, context, extraAuthParams ) {
-    var stateToEncode = { 'jiveRedirectUrl' : callback };
-    if ( context ) {
+/**
+ * Requests an access token by oauth access code (oauth access code is given in registration requests and valid for few minutes).
+ * @param jiveUrl - the url of the jive community. this function will use this url to find the community in the persistence and get the client id and secret.
+ * @param oauthCode - the code needed to be use to get access to a specific registration scope (usually a group).
+ * @returns a promise for success and failure [use .then(...) and .catch(...)]
+ */
+exports.requestAccessToken = function (jiveUrl, oauthCode) {
+    var defer = q.defer();
+
+    community.findByJiveURL(jiveUrl)
+        .then(function (communityObj) {
+            if (communityObj) {
+                var oauthConf = {
+                    client_id: communityObj.clientId,
+                    client_secret: communityObj.clientSecret,
+                    code: oauthCode,
+                    jiveUrl: jiveUrl
+                };
+                tileClient.requestAccessToken(oauthConf, defer.resolve, defer.reject);
+            }
+            else {
+                defer.reject(new Error("No community found by the url: " + jiveUrl));
+            }
+        })
+        .catch(defer.reject);
+
+    return defer.promise;
+};
+
+exports.buildAuthorizeUrlResponseMap = function (oauth2Conf, callback, context, extraAuthParams) {
+    var stateToEncode = { 'jiveRedirectUrl': callback };
+    if (context) {
         stateToEncode = util._extend(stateToEncode, context);
     }
 
-    var url  = oauth2Conf['originServerAuthorizationUrl'] + "?" +
-        "state="            + jive.util.base64Encode(JSON.stringify(stateToEncode)) +
-        "&redirect_uri="    + oauth2Conf['clientOAuth2CallbackUrl'] +
-        "&client_id="       + oauth2Conf['oauth2ConsumerKey'] +
-        "&response_type="   + "code";
+    var url = oauth2Conf['originServerAuthorizationUrl'] + "?" +
+        "state=" + jive.util.base64Encode(JSON.stringify(stateToEncode)) +
+        "&redirect_uri=" + oauth2Conf['clientOAuth2CallbackUrl'] +
+        "&client_id=" + oauth2Conf['oauth2ConsumerKey'] +
+        "&response_type=" + "code";
 
-    if ( extraAuthParams ) {
+    if (extraAuthParams) {
         var extraAuthStr = '';
         for (var key in extraAuthParams) {
             if (extraAuthParams.hasOwnProperty(key)) {
@@ -26,37 +58,37 @@ exports.buildAuthorizeUrlResponseMap = function( oauth2Conf, callback, context, 
     }
 
     return {
-        'url' : url
+        'url': url
     };
 };
 
-exports.buildOauth2CallbackObject = function(oauth2Conf, code, extraParams ) {
+exports.buildOauth2CallbackObject = function (oauth2Conf, code, extraParams) {
 
     var postObject = {
-        'grant_type'        : 'authorization_code',
-        'redirect_uri'      : oauth2Conf['clientOAuth2CallbackUrl'],
-        'client_id'         : oauth2Conf['oauth2ConsumerKey'],
-        'client_secret'     : oauth2Conf['oauth2ConsumerSecret'],
-        'code'              : code
+        'grant_type': 'authorization_code',
+        'redirect_uri': oauth2Conf['clientOAuth2CallbackUrl'],
+        'client_id': oauth2Conf['oauth2ConsumerKey'],
+        'client_secret': oauth2Conf['oauth2ConsumerSecret'],
+        'code': code
     };
 
-    if ( extraParams ) {
+    if (extraParams) {
         postObject = util._extend(postObject, extraParams);
     }
 
     return postObject;
 };
 
-exports.refreshTokenFlow = function(oauth2Conf, refreshToken) {
+exports.refreshTokenFlow = function (oauth2Conf, refreshToken) {
     var postObject = {
-        'grant_type'        : 'refresh_token',
-        'refresh_token'     : refreshToken,
-        'client_id'         : oauth2Conf['oauth2ConsumerKey'],
-        'client_secret'     : oauth2Conf['oauth2ConsumerSecret']
+        'grant_type': 'refresh_token',
+        'refresh_token': refreshToken,
+        'client_id': oauth2Conf['oauth2ConsumerKey'],
+        'client_secret': oauth2Conf['oauth2ConsumerSecret']
     };
 
     var headers = { 'Content-Type': 'application/x-www-form-urlencoded' };
 
-    return jive.util.buildRequest( oauth2Conf['originServerTokenRequestUrl'], 'POST', postObject, headers);
+    return jive.util.buildRequest(oauth2Conf['originServerTokenRequestUrl'], 'POST', postObject, headers);
 };
 
