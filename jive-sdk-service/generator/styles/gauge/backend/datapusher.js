665e56cf0bbda93f3f3d6e2e9ca3c656de34bd24
Aron Racho <aron.racho@jivesoftware.com>
2013-10-18 20:32:59 -0700

    improving the examples

--- jive-sdk-service/generator/styles/gauge/backend/datapusher.js
+++ jive-sdk-service/generator/styles/gauge/backend/datapusher.js
@@ -16,60 +16,95 @@
 
 var jive = require("jive-sdk");
 
-exports.task = function() {
+function processTileInstance(instance) {
     var statusNames = [ "Poor", "Fair", "Good", "Excellent", "Outstanding" ];
+    jive.logger.debug('running pusher for ', instance.name, 'instance', instance.id);
+
+    var level = Number(instance.config.level);
+    var dataToPush = {
+        data: {
+            "message": "Simple Gauge",
+            "sections": createSections(5),
+            "activeIndex": level,
+            "status": statusNames[level]
+        }
+    };
+
+    jive.tiles.pushData(instance, dataToPush);
+}
+
+function pushData() {
 
     jive.tiles.findByDefinitionName( '{{{TILE_NAME}}}' ).then( function(instances) {
         if ( instances ) {
             instances.forEach( function( instance ) {
-                jive.logger.debug('running pusher for ', instance.name, 'instance', instance.id );
-
-                var level = Number(instance.config.level);
-                var dataToPush = {
-                    data : {
-                        "message" : "Simple Gauge",
-                        "sections" : createSections(5),
-                        "activeIndex": level,
-                        "status" : statusNames[level]
-                    }
-                };
-
-                jive.tiles.pushData( instance, dataToPush);
+                processTileInstance(instance);
             });
         }
     });
+}
 
-    function createSections(numSections) {
-        var sections = [];
-        for (var i = 0; i < numSections; i++) {
-            sections.push({
-                "label": "dummy",
-                "color": getSectionColor(i, numSections)
-            });
-        }
-        return sections;
+function createSections(numSections) {
+    var sections = [];
+    for (var i = 0; i < numSections; i++) {
+        sections.push({
+            "label": "dummy",
+            "color": getSectionColor(i, numSections)
+        });
+    }
+    return sections;
+}
+
+function getSectionColor(secIndex, numSections) {
+    var maxGreen = 187,
+        blueString = "00",
+        red, green;
+    if (secIndex < ((numSections - 1) / 2)) {
+        red = 255;
+        green = Math.round((255 * ((secIndex) / ((numSections - 1) / 2))));
+    }
+    else if (secIndex > ((numSections - 1) / 2)) {
+        green = maxGreen;
+        red = Math.round((255 * (1 - ((secIndex - ((numSections - 1) / 2)) / ((numSections - 1) / 2)))));
+    }
+    else {
+        red = 220;
+        green = 220;
     }
 
-    function getSectionColor(secIndex, numSections) {
-        var maxGreen = 187,
-            blueString = "00",
-            red, green;
-        if (secIndex < ((numSections - 1) / 2)) {
-            red = 255;
-            green = Math.round((255 * ((secIndex) / ((numSections - 1) / 2))));
-        }
-        else if (secIndex > ((numSections - 1) / 2)) {
-            green = maxGreen;
-            red = Math.round((255 * (1 - ((secIndex - ((numSections - 1) / 2)) / ((numSections - 1) / 2)))));
-        }
-        else {
-            red = 220;
-            green = 220;
-        }
+    var redString = (red < 16 ? "0" + red.toString(16) : red.toString(16)),
+        greenString = (green < 16 ? "0" + green.toString(16) : green.toString(16));
 
-        var redString = (red < 16 ? "0" + red.toString(16) : red.toString(16)),
-            greenString = (green < 16 ? "0" + green.toString(16) : green.toString(16));
+    return '#' + redString + greenString + blueString;
+}
 
-        return '#' + redString + greenString + blueString;
+/**
+ * Schedules the tile update task to automatically fire every 10 seconds
+ */
+exports.task = [
+    {
+        'interval' : 10000,
+        'handler' : pushData
     }
-};
+];
+
+/**
+ * Defines event handlers for the tile life cycle events
+ */
+exports.eventHandlers = [
+
+    // process tile instance whenever a new one is registered with the service
+    {
+        'event' : jive.constants.globalEventNames.NEW_INSTANCE,
+        'handler' : processTileInstance
+    },
+
+    // process tile instance whenever an existing tile instance is updated
+    {
+        'event' : jive.constants.globalEventNames.INSTANCE_UPDATED,
+        'handler' : processTileInstance
+    }
+];
+
+
+
