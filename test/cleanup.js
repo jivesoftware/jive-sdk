4e6d9bece9a735c30d26308e354bd9da1c4803a2
Aron Racho <aron.racho@jivesoftware.com>
2013-11-24 16:34:04 -0800

    tests, fixes

--- test/cleanup.js
+++ test/cleanup.js
@@ -1,9 +1,11 @@
 var testUtils = require('./util/testUtils');
+var path = process.argv[2];
 
-testUtils.fsread( 'coverage.html' ).then( function(data) {
+console.log(path);
+testUtils.fsread( path ).then( function(data) {
     var text = new Buffer(data).toString();
     var text = text.split('<!DOCTYPE html>')[1];
     return text;
 }).then( function(text) {
-    testUtils.fswrite( text, 'coverage.html');
+    testUtils.fswrite( text, path);
 });
