645a77585c8a8cd8df7beb3121cb64dd727875c1
Pablo Addante <pablo.addante@jivesoftware.com>
2013-11-25 14:13:25 -0800

    More jive.util tests.

--- test/unit/jiveutil/testcases/fsUtils.js
+++ test/unit/jiveutil/testcases/fsUtils.js
@@ -43,7 +43,7 @@ describe('jive', function () {
             var jive = this['jive'];
             jive.util.fsGetSize(testUtils.getResourceFilePath('sizetestdir')).then(
                 function ( size ) {
-                    assert.equal(size, 136, 'Wrong size.');
+                    assert.notEqual(size, 0, 'Wrong size.');
                     done();
                 }
             );
@@ -104,6 +104,70 @@ describe('jive', function () {
 
     describe('#util.fsrename()', function () {
 
+        it('rename a directory', function (done) {
+            var jive = this['jive'];
+            testUtils.createTempDir().then(
+                function ( tmpdir ) {
+                    var newDirName = testUtils.getResourceFilePath('renamed-test-dir-' + testUtils.guid());
+                    jive.util.fsrename(tmpdir, newDirName).then(
+                        function ( response ) {
+                            //succeeded, let's do some extra checks
+                            jive.util.fsisdir(newDirName).then(
+                                function ( isdir ) {
+                                    assert.equal(isdir, true, 'Directory was not renamed');
+                                    if ( isdir ) {
+                                        jive.util.fsrmdir(newDirName).then(
+                                            // nothing
+                                        );
+                                    }
+                                    else {
+                                        jive.util.fsrmdir(tmpdir).then(
+                                            // nothing
+                                        );
+                                    }
+                                    done()
+                                }
+                            )
+                        }
+                    );
+                }
+            );
+        });
+
+        it('rename to an existing directory using "force"', function (done) {
+            var jive = this['jive'];
+            var newDirName = testUtils.getResourceFilePath('tmp-dir');
+            testUtils.createTempDir().then(
+                function ( tmpdir ) {
+                    jive.util.fsmkdir(newDirName).then(
+                        function( response ) {
+                            jive.util.fsrename(tmpdir, newDirName, true).then(
+                                function ( response ) {
+                                    //succeeded, let's do some extra checks
+                                    jive.util.fsisdir(newDirName).then(
+                                        function ( isdir ) {
+                                            assert.equal(isdir, true, 'Directory was not renamed');
+                                            if ( isdir ) {
+                                                jive.util.fsrmdir(newDirName).then(
+                                                    // nothing
+                                                );
+                                            }
+                                            else {
+                                                jive.util.fsrmdir(tmpdir).then(
+                                                    // nothing
+                                                );
+                                            }
+                                            done()
+                                        }
+                                    )
+                                }
+                            );
+                        }
+                    );
+                }
+            );
+        });
+
     });
 
     describe('#util.fsmkdir()', function () {
@@ -119,6 +183,37 @@ describe('jive', function () {
     });
 
     describe('#util.fsreaddir()', function () {
+
+        it('test with an existing directory', function (done) {
+            var jive = this['jive'];
+            jive.util.fsreaddir(testUtils.getResourceFilePath('sizetestdir')).then(
+                function ( size ) {
+                    assert.notEqual(size, 0, 'Wrong size.');
+                    done();
+                }
+            );
+
+        });
+
+        it('test with a non-existing directory', function (done) {
+            var jive = this['jive'];
+            jive.util.fsreaddir(testUtils.getResourceFilePath('dummy-folder/')).then(
+                function ( response ) {
+                    assert.equal(response, undefined, 'Wrong read value.');
+                    done();
+                }
+            );
+        });
+
+        it('test with a file', function (done) {
+            var jive = this['jive'];
+            jive.util.fsreaddir(testUtils.getResourceFilePath('loremipsum.txt')).then(
+                function ( response ) {
+                    assert.equal(response, undefined, 'Wrong read value.');
+                    done();
+                }
+            );
+        });
 
     });
 
