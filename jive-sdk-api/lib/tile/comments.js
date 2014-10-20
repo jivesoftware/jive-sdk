80b498014434b2b455e71e353cd1e98b052f117f
longstone <git@longstone.ch>
2014-10-20 09:40:18 +0200

    added fetchCommentsOnActivityByExternalID

--- jive-sdk-api/lib/tile/comments.js
+++ jive-sdk-api/lib/tile/comments.js
@@ -29,28 +29,28 @@ var q = require('q');
  * @param comment
  * @return {Promise} Promise
  */
-exports.commentOnActivity = function(jiveActivity, comment) {
+exports.commentOnActivity = function (jiveActivity, comment) {
     if (!(jiveActivity && jiveActivity.resources && jiveActivity.resources.comments && jiveActivity.resources.comments.ref)
         || !jiveActivity.parent) {
 
-        return q.reject({ 'err' : 'Error in jive.extstreams.commentOnActivity: input activity is not a valid Jive object.' +
-            'It is missing the resources.comments.ref field or parent field.'} );
+        return q.reject({ 'err': 'Error in jive.extstreams.commentOnActivity: input activity is not a valid Jive object.' +
+            'It is missing the resources.comments.ref field or parent field.'});
     }
     var commentsURL = jiveActivity.resources.comments.ref;
     var parentInstanceURL = jiveActivity.parent + '/activities';
 
-    return jive.extstreams.findByURL(parentInstanceURL).then(function(extstream) {
-        if (!comment.externalID){
+    return jive.extstreams.findByURL(parentInstanceURL).then(function (extstream) {
+        if (!comment.externalID) {
             comment.externalID = extstream.name + '_' + 'comment' + '_' + String(new Date().getTime());
-            jive.logger.warn(jive.util.format('No externalID field given when creating new comment. Assigning ID %s', comment.externalID ));
+            jive.logger.warn(jive.util.format('No externalID field given when creating new comment. Assigning ID %s', comment.externalID));
         }
 
         // schedule the comment push
         return jive.context.scheduler.schedule(jive.constants.tileEventNames.PUSH_COMMENT_TO_JIVE, {
-            'tileInstance' : extstream,
-            'comment' : comment,
-            'commentsURL' : commentsURL
-        } );
+            'tileInstance': extstream,
+            'comment': comment,
+            'commentsURL': commentsURL
+        });
     });
 };
 
@@ -62,20 +62,20 @@ exports.commentOnActivity = function(jiveActivity, comment) {
  * @param comment
  * @return {Promise} Promise
  */
-exports.commentOnActivityByExternalID = function(extstream, externalActivityID, comment) {
+exports.commentOnActivityByExternalID = function (extstream, externalActivityID, comment) {
     var dataURL = extstream['url'];
     var commentsURL = dataURL.replace(/activities$/, 'extactivities/') + externalActivityID + '/comments';
-    if (!comment.externalID){
+    if (!comment.externalID) {
         comment.externalID = extstream.name + '_' + 'comment' + '_' + String(new Date().getTime());
-        jive.logger.warn(jive.util.format('No externalID field given when creating new comment. Assigning ID %s', comment.externalID ));
+        jive.logger.warn(jive.util.format('No externalID field given when creating new comment. Assigning ID %s', comment.externalID));
     }
 
     // schedule the comment push
     return jive.context.scheduler.schedule(jive.constants.tileEventNames.PUSH_COMMENT_TO_JIVE, {
-        'tileInstance' : extstream,
-        'comment' : comment,
-        'commentsURL' : commentsURL
-    } );
+        'tileInstance': extstream,
+        'comment': comment,
+        'commentsURL': commentsURL
+    });
 };
 
 /**
@@ -85,24 +85,47 @@ exports.commentOnActivityByExternalID = function(extstream, externalActivityID, 
  * @param opts commentSourceType can be JIVE or EXTERNAL.
  * @return {Promise} Promise
  */
-exports.fetchCommentsOnActivity = function(jiveActivity, opts) {
+exports.fetchCommentsOnActivity = function (jiveActivity, opts) {
     if (!(jiveActivity && jiveActivity.resources && jiveActivity.resources.comments && jiveActivity.resources.comments.ref)
         || !jiveActivity.parent) {
 
-        return q.reject({ 'err' : 'Error in jive.extstreams.fetchCommentsOnActivity: ' +
-            'input activity is not a valid Jive object. It is missing the resources.comments.ref field or parent field.'} );
+        return q.reject({ 'err': 'Error in jive.extstreams.fetchCommentsOnActivity: ' +
+            'input activity is not a valid Jive object. It is missing the resources.comments.ref field or parent field.'});
     }
 
     var commentsURL = jiveActivity.resources.comments.ref;
     commentsURL += buildQueryString(opts['fieldList'], opts['itemsPerPage'], opts['commentSourceType']);
     var parentInstanceURL = jiveActivity.parent + '/activities';
 
-    return jive.extstreams.findByURL(parentInstanceURL).then( function(extstream) {
+    return jive.extstreams.findByURL(parentInstanceURL).then(function (extstream) {
         return jive.context.scheduler.schedule(jive.constants.tileEventNames.GET_PAGINATED_RESULTS, {
-            'extstream' : extstream,
-            'commentsURL' : commentsURL
-        } );
-    }).then(function(response){
+            'extstream': extstream,
+            'commentsURL': commentsURL
+        });
+    }).then(function (response) {
+        if (response.entity && response.entity.list) {
+            response.entity.list = filterComments(response.entity.list, opts['commentSourceType'], opts['publishedAfter']);
+        }
+        return response;
+    });
+};
+
+/**
+ * Fetches all comments associated with a jive activity item by external ID. Can be filtered for only Jive originated comments,
+ * or for those originating outside of Jive (external comments).
+ * @param extstream
+ * @param externalActivityID
+ * @param opts commentSourceType can be JIVE or EXTERNAL.
+ * @return {Promise} Promise
+ */
+exports.fetchCommentsOnActivityByExternalID = function (extstream, externalActivityID, opts) {
+    var dataURL = extstream['url'];
+    var commentsURL = dataURL.replace(/activities$/, 'extactivities/') + externalActivityID + '/comments';
+    commentsURL += buildQueryString(opts['fieldList'], opts['itemsPerPage'], opts['commentSourceType']);
+    return jive.context.scheduler.schedule(jive.constants.tileEventNames.GET_PAGINATED_RESULTS, {
+        'extstream': extstream,
+        'commentsURL': commentsURL
+    }).then(function (response) {
         if (response.entity && response.entity.list) {
             response.entity.list = filterComments(response.entity.list, opts['commentSourceType'], opts['publishedAfter']);
         }
@@ -115,7 +138,7 @@ exports.fetchCommentsOnActivity = function(jiveActivity, opts) {
  * @param opts
  * @return {Promise} Promise
  */
-exports.fetchAllCommentsForExtstream = function(extstream, opts) {
+exports.fetchAllCommentsForExtstream = function (extstream, opts) {
 
     var dataURL = extstream['url'];
     var commentsURL = commentsUrlFromDataUrl(dataURL);
@@ -123,11 +146,11 @@ exports.fetchAllCommentsForExtstream = function(extstream, opts) {
     commentsURL += buildQueryString(opts['fieldList'], opts['itemsPerPage'], opts['commentSourceType']);
 
     var promise = jive.context.scheduler.schedule(jive.constants.tileEventNames.GET_PAGINATED_RESULTS, {
-        'extstream' : extstream,
-        'commentsURL' : commentsURL
-    } );
+        'extstream': extstream,
+        'commentsURL': commentsURL
+    });
 
-    return promise.then(function(response) {
+    return promise.then(function (response) {
 
         if (response.entity && response.entity.list) {
             response.entity.list = filterComments(response.entity.list, opts['commentSourceType'], opts['publishedAfter']);
@@ -150,7 +173,7 @@ function buildQueryString(optionalFieldList, optionalItemsPerPage, commentSource
             if (optionalFieldList.indexOf('published') < 0) { //Need publish date for filtering
                 optionalFieldList.push('published');
             }
-            queryStr +=  'fields=' + encodeURIComponent(optionalFieldList.join(','));
+            queryStr += 'fields=' + encodeURIComponent(optionalFieldList.join(','));
             q = true;
         }
         if (optionalItemsPerPage) {
@@ -177,19 +200,19 @@ function filterComments(list, commentSourceType, publishedAfter) {
     if (commentSourceType && commentSourceType.toUpperCase() != 'ALL') {
 
         if (commentSourceType.toUpperCase() == 'JIVE') {
-            list = list.filter(function(comment) {
+            list = list.filter(function (comment) {
                 return comment.externalID == undefined;
-            }) ;
+            });
         }
         else if (commentSourceType.toUpperCase() == 'EXTERNAL') {
-            list = list.filter(function(comment) {
+            list = list.filter(function (comment) {
                 return comment.externalID != undefined;
-            }) ;
+            });
         }
     }
 
     if (publishedAfter) {
-        list = list.filter(function(comment) {
+        list = list.filter(function (comment) {
             var published = new Date(comment['published']);
             if (isNaN(published.getTime())) {
                 return true;
